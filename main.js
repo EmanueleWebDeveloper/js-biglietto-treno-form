4ece8e037f5c7a05fe01b0cdf62652d8bdb90e15
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-09 15:08:57 +0100

    modifiche

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-function calcolaPrezzo() {
-    const costoKilometro = 0.21;
-    const risultatoHtml = document.getElementById('risultato');
+const prezzoKm = 0.21;
+let numeroKm = document.getElementById("numero-km");
+let eta = document.getElementById("eta");
+let nomePasseggero = document.getElementById("nome")
 
-    const nomeCognome = document.getElementById('NomeeCognome').value;
-    const numeroKilometri = parseFloat(document.getElementById('numeroKilometri').value);
-    const etaPasseggero = parseInt(document.getElementById('etaPasseggero').value);
+function calcoloPrezzo() {
 
-    let prezzoBiglietto = costoKilometro * numeroKilometri;
-
-    if (etaPasseggero < 18) {
-        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 0.2);
-        risultatoHtml.value = `Ciao ${nomeCognome}, il prezzo del biglietto è: ${prezzoBiglietto.toFixed(2)}€ con uno sconto del 20% perché sei minorenne.`;
-    } else if (etaPasseggero >= 65) {
-        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 0.4);
-        risultatoHtml.value = `Ciao ${nomeCognome}, il prezzo del biglietto è: ${prezzoBiglietto.toFixed(2)}€ con uno sconto del 40% perché sei over 65.`;
+    if (eta.value == "minorenne") {
+        document.getElementById("prezzo").innerHTML = (0.8 * (prezzoKm * numeroKm.value)).toFixed(2) + " " + "€";
+        document.getElementById("offerta").innerHTML = "Offerta under 18";
+    } else if (eta.value == "over") {
+        document.getElementById("prezzo").innerHTML = (0.6 * (prezzoKm * numeroKm.value)).toFixed(2) + " " + "€";
+        document.getElementById("offerta").innerHTML = "Offerta over 65";
     } else {
-        risultatoHtml.value = `Ciao ${nomeCognome}, il prezzo del biglietto è: ${prezzoBiglietto.toFixed(2)}€.`;
+        document.getElementById("prezzo").innerHTML = (prezzoKm * numeroKm.value).toFixed(2) + " " + "€";
+        document.getElementById("offerta").innerHTML = "Biglietto Standard";
     }
+
+    document.getElementById("numero-carrozza").innerHTML = numeroCarrozza(1, 8)
+
+    document.getElementById("codice-cp").innerHTML = codiceCp()
+
+    document.getElementById("nome-passeggero").innerHTML = nomePasseggero.value
+
+    document.getElementById("biglietto").style.display = "block";
+
 }
+
+const bottoneHtml = document.getElementById("button-calc");
+
+bottoneHtml.addEventListener("click", calcoloPrezzo)
+
+
+function numeroCarrozza(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function codiceCp() {
+    return Math.floor(Math.random() * 90000 + 10000)
+}
+
+
+const bottoneAnnulla = document.getElementById("annulla");
+const refreshPage = () => {
+    location.reload();
+}
+
+bottoneAnnulla.addEventListener("click", refreshPage)
+
+
+// console.log ( nomeCognomeHtml, KmHtml, etaHtml )
